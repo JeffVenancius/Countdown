36f179aab58bcd8c30b6d4b82b044c8a2602e861
JeffVenancius <jefferson.venancius@gmail.com>
2022-04-07 14:10:01 -0300

    first commit

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-const finishLine = {day: 28, hour: 22, minutes: 30, seconds: 60}
+const finishLine = {days: 28, hours: 22, minutes: 30, seconds: 60}
 
-seconds();minutes();hours();days()
-setInterval(seconds, 1000);
+timeUpdate(); timeUpdate('minutes'); timeUpdate('hours'); timeUpdate('days');
+setInterval(timeUpdate, 1000);
 
-function seconds(){
+function timeUpdate(selector = 'seconds'){
+	const select = document.querySelector('#' + selector + ' ' +'h3');
 	let currentPeriod = new Date();
-	const seconds = document.querySelector('#seconds h3')
-	let currentSeconds = currentPeriod.getSeconds();
-	seconds.innerHTML =  (60 - currentSeconds) - (60 - finishLine.seconds);
-	if (currentSeconds == 0 || currentSeconds == 60){minutes()}
+	let getTime;
+	let finishTime;
+
+	switch (selector[0]) {
+		case 's':
+			getTime = currentPeriod.getSeconds();
+			finishTime = finishLine.seconds;
+			break;
+		case 'm':
+			getTime = currentPeriod.getMinutes();
+			finishTime = finishLine.minutes;
+			break;
+		case 'h':
+			getTime = currentPeriod.getHours();
+			finishTime = finishLine.hours;
+			break;
+		case 'd':
+			getTime = currentPeriod.getDate();
+			finishTime = finishLine.days;
+			break;
+	}
+	
+	let last = getTime - finishTime;
+	if (last < 0) {last = finishTime - getTime};
+	select.innerHTML =  String(last).padStart(2,'0');
+
+	if (getTime == 0) {
+		switch (selector[0]) {
+			case 's':timeUpdate('minutes'); break;
+			case 'm':timeUpdate('hours'); break;
+			case 'h':timeUpdate('days'); break;
+		}
+	}
 }
 
-function minutes(){
-	let currentPeriod = new Date();
-	const minutes = document.querySelector('#minutes h3')
-	let currentMinutes = currentPeriod.getMinutes();
-	minutes.innerHTML = (60 - currentMinutes) - (60 - finishLine.minutes)
-	if (currentMinutes == 0 || currentMinutes == 60){hours()}}
-
-function hours(){
-	let currentPeriod = new Date();
-	const hours = document.querySelector('#hours h3')
-	let currentHour = currentPeriod.getHours();
-	hours.innerHTML =  (60 - currentHour) - (60 - finishLine.hour)
-	if (currentHour == 0 || currentHour == 24){days()}}
-
-function days(){
-	let currentPeriod = new Date();
-	const days = document.querySelector('#days h3')
-	let currentDay = currentPeriod.getDate();
-	days.innerHTML = finishLine.day - currentDay 
-}
